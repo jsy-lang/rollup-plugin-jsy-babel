b0d42d6a0419baacae6d99a7e1eb8ad5817e7527
Shane Holloway <shane.holloway@ieee.org>
2018-02-26 12:41:32 -0700

    Changed to prepend jsy_preset to conifg.presets

--- code/index.js
+++ code/index.js
@@ -2,14 +2,16 @@ import rpi_babel from 'rollup-plugin-babel'
 
 const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ]
 jsy_babel.jsy_preset = jsy_preset
-const default_config = {
-  exclude: 'node_modules/**',
-  presets: [ jsy_preset ],
-  plugins: [],
-  babelrc: false }
 
-export default function jsy_babel(config=default_config) {
-  const res = rpi_babel(config)
-  res.name = 'jsy-babel'
-  return res
+const default_config = { exclude: 'node_modules/**', babelrc: false }
+  // presets: [ jsy_preset ].concat(config.presets || []),
+  // plugins: [],
+
+export default function jsy_babel(config) {
+  config = Object.assign({}, default_config, config)
+  config.presets = [jsy_babel.jsy_preset].concat(config.presets || [])
+
+  const rpi = rpi_babel(config)
+  rpi.name = 'jsy-babel'
+  return rpi
 }
--- dist/rollup-plugin-jsy-babel.cjs.js
+++ dist/rollup-plugin-jsy-babel.cjs.js
@@ -6,16 +6,18 @@ var rpi_babel = _interopDefault(require('rollup-plugin-babel'));
 
 const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ];
 jsy_babel.jsy_preset = jsy_preset;
-const default_config = {
-  exclude: 'node_modules/**',
-  presets: [ jsy_preset ],
-  plugins: [],
-  babelrc: false };
 
-function jsy_babel(config=default_config) {
-  const res = rpi_babel(config);
-  res.name = 'jsy-babel';
-  return res
+const default_config = { exclude: 'node_modules/**', babelrc: false };
+  // presets: [ jsy_preset ].concat(config.presets || []),
+  // plugins: [],
+
+function jsy_babel(config) {
+  config = Object.assign({}, default_config, config);
+  config.presets = [jsy_babel.jsy_preset].concat(config.presets || []);
+
+  const rpi = rpi_babel(config);
+  rpi.name = 'jsy-babel';
+  return rpi
 }
 
 module.exports = jsy_babel;
