939d7f352366dc8dbb76797dfab000c902ceac40
Shane Holloway <shane.holloway@ieee.org>
2018-05-09 10:55:50 -0600

    Bugfix for .env support

--- dist/rollup-plugin-jsy-babel.cjs.js
+++ dist/rollup-plugin-jsy-babel.cjs.js
@@ -15,7 +15,7 @@ function jsy_babel(config) {
   config = Object.assign({}, default_config, config);
 
   const jsy = jsy_babel.jsy_preset.slice();
-  jsy[1] = Object.assign({}, jsy[1], config.jsy);
+  jsy[1] = Object.assign({}, jsy[1], config.env, config.jsy);
 
   config.presets = [jsy].concat(config.presets || []);
 
